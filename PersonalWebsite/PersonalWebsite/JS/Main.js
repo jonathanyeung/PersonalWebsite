2cb41cfaf3980489dd092a6c1ed78e9d8fa3e69b
Jonathan Yeung <jonathanyeung@outlook.com>
2015-06-08 15:56:55 -0700

    Initial Major Revision
    Adding JS support files from sandbox project
    Adding resource files
    Adding CSS
    Modifying Core Index HTML.

--- PersonalWebsite/PersonalWebsite/JS/Main.js
+++ PersonalWebsite/PersonalWebsite/JS/Main.js
@@ -1,5 +1,31 @@
-﻿$(document).ready(function () {
-    $("#check-me-out-button").click(function () {
-        this.innerText = "New Text";
+﻿$(window).load(function () {
+    // Set up event handlers for clicks on the navigation header
+    $('#SkillsNavBarLi').click(function () {
+        document.getElementById('SkillsSection').scrollIntoView();
     });
+
+    $('#ExperienceNavBarLi').click(function () {
+        document.getElementById('ExperienceSection').scrollIntoView();
+    });
+
+    $('#projectsNavBarLi').click(function () {
+        document.getElementById('ProjectsSection').scrollIntoView();
+    });
+
+    $('#animationsNavBarLi').click(function () {
+        document.getElementById('ContactSection').scrollIntoView();
+    });
+
+    $("#check-me-out-button").click(function () {
+        document.getElementById('SkillsSection').scrollIntoView();
+    });
+
+    carousel.initialize();
 });
+
+
+
+$('#SkillsNavBarLi').removeClass("emphasized");
+$('#ExperienceNavBarLi').removeClass("emphasized");
+$('#projectsNavBarLi').removeClass("emphasized");
+$('#animationsNavBarLi').removeClass("emphasized");
---
+++ PersonalWebsite/PersonalWebsite/JS/NavBar.js
@@ -0,0 +1,51 @@
+﻿$(window).scroll(function () {
+
+    //ToDo: Look up enums in JavaScript
+    var currentSection = 1;
+
+    if ($(this).scrollTop() > 1) {
+        $('header').addClass("sticky");
+    }
+    else {
+        $('header').removeClass("sticky");
+    }
+
+    if ($(this).scrollTop() > $('#SkillsSection').offset().top - $('header.sticky').height()) { // ToDo: Figure out if .sticky here is right or if it actually does anything.
+        currentSection = 1;
+    }
+
+    if ($(this).scrollTop() > $('#ExperienceSection').offset().top - $('header.sticky').height()) {
+        currentSection = 2;
+    }
+
+    if ($(this).scrollTop() > $('#ProjectsSection').offset().top - $('header.sticky').height()) {
+        currentSection = 3;
+    }
+
+    // ToDo: Fix this such that contact is displayed when the user has scrolled all the way to the bottom of the page.
+    if ($(this).scrollTop() > $('#ContactSection').offset() + $('#ContactSection').height()) {
+        currentSection = 4;
+    }
+
+    $('#SkillsNavBarLi').removeClass("emphasized");
+    $('#ExperienceNavBarLi').removeClass("emphasized");
+    $('#projectsNavBarLi').removeClass("emphasized");
+    $('#animationsNavBarLi').removeClass("emphasized");
+
+    if (currentSection == 1) {
+        $('#SkillsNavBarLi').addClass("emphasized");
+    }
+
+    else if (currentSection == 2) {
+        $('#ExperienceNavBarLi').addClass("emphasized");
+    }
+
+    else if (currentSection == 3) {
+        $('#projectsNavBarLi').addClass("emphasized");
+    }
+
+    else if (currentSection == 4) {
+        $('#animationsNavBarLi').addClass("emphasized");
+    }
+
+});
